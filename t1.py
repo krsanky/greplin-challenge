786f4bed77f7f9f94858646d1b55b19e9d4089d9
Paul Wisehart <paul@1ud2.com>
2010-10-08 22:12:01 -0400

    t1 (1st question) done

--- t1.py
+++ t1.py
@@ -1,3 +1,38 @@
 #!/usr/bin/env python
 
-print "q1"
+text = open('gettysburg.txt', 'r').read().lower()
+
+def all_strs_of_length(text, length):
+    """
+    get all consecutive strings of length "length"
+    in the text,
+    return list
+    """
+    text_len = len(text)
+    if text_len < length:
+        return None
+
+    strs = []
+    idx = 0
+    for i in range(len(text)):
+        if (text_len - i) < length:
+            #print 'break'
+            break
+        strs.append(text[i:i+length])
+
+    return strs
+
+def longest_rev_str(text):
+    """
+    return the longest substring that is the same in reverse
+
+    returns 1st longest, not all longest
+    """
+    for i in range(len(text),1,-1):
+        print 'str-len:',i
+        strs = all_strs_of_length(text, i)
+        for s in strs:
+            if s == s[::-1]: return s
+
+if __name__ == '__main__':
+    print str(longest_rev_str(text))
