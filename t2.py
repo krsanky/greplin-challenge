f834906c3dc34b3e13063af37984b34bfb0f5a74
Paul Wisehart <paul@1ud2.com>
2010-10-08 22:20:36 -0400

    add t2

--- t2.py
+++ t2.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
 
+"""
+Level 2
+
+	----------------------------------------
+
+	Congratulations.  You have reached level 2.
+
+	To get the password for level 3, write code to find the first prime
+	fibonacci number larger than a given minimum.  For example, the first
+	prime fibonacci number larger than 10 is 13.
+
+	When you are ready, go here or call this automated
+	number (415) 799-9454.
+
+	You will receive additional instructions at that time.  For the second portion
+	of this task, note that for the number 12 we consider the sum of the prime divisors
+	to be 2 + 3 = 5.  We do not include 2 twice even though it divides 12 twice.
+"""
+
 if __name__ == '__main__':
     print "question 2"
