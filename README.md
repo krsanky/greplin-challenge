b3adaa252b190fffa8cdfdf80157d3b6a5f91560
Paul Wisehart <paul@1ud2.com>
2010-10-09 15:20:04 -0400

    add README.md, and setup a new clojure file for level1 (rename t1.py to level1.py to match)

---
+++ README.md
@@ -0,0 +1,1 @@
+http://challenge.greplin.com/
---
+++ level1.clj
@@ -0,0 +1,9 @@
+;; Level 1
+;;
+;; ----------------------------------------
+;;
+;; Embedded in this block of text is the password for level 2.
+;; The password is the longest substring that is the same in reverse.
+;;
+;; As an example, if the input was "I like racecars that go fast"
+;; the password would be "racecar".
