82020536c2911f7d5c8cfe944b3be736496b4736
Paul Wisehart <paul@1ud2.com>
2010-10-10 07:53:36 -0400

    level-1 works in clojure, but slowly

--- greplin-challenge/src/greplin_challenge/level1.clj
+++ greplin-challenge/src/greplin_challenge/level1.clj
@@ -10,5 +10,39 @@
 ;; As an example, if the input was "I like racecars that go fast"
 ;; the password would be "racecar".
 
+(def getty-txt (.toLowerCase (slurp "gettysburg.txt")))
 
-(print "asd")
+(defn substrs-of-len [txt len]
+  "get all consecutive strings of length 'len' in 'txt'
+   returns a list of strings"
+  (map (fn [s] (apply str s)) (partition len 1 txt))) ;;change this to clojure's lambda shorthand ...
+
+(defn str-is-rev? [s] ;; what if s is nil ?
+  "tests if the string equals the reverse string, (and returns it if true)"
+  (let [rev-str (apply str (reverse s))]
+    (if (= s rev-str)
+      true
+      false)))
+
+(defn find-rev-str [txt len]
+  "find a reversible string of length 'len' in 'txt'"
+  (let [strs (substrs-of-len txt len)]
+    (let [ret (first (filter str-is-rev? strs))]
+      (println (str ret ":" len))
+      ret)))
+
+(defn longest-rev-str [txt]
+  "find the longest reversible substring"
+  (loop [len (count txt)]
+    (let [str (find-rev-str txt len)]
+      (if str
+        str
+        (recur (dec len))))))
+(println (longest-rev-str getty-txt))
+;;(println (longest-rev-str "abba123"))
+
+;; (print (str getty-text "\n"))
+;; (substrs-of-len "asdasdasdABBAasdasdasd" 4)
+;; (print (str "count:" (count getty-text)))
+
+
