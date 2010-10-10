6c091b5d8d75cda8f8cd5a77eedb89d679c65534
Paul Wisehart <paul@1ud2.com>
2010-10-10 08:53:06 -0400

    rename

--- level2.py
+++ level2.py
@@ -3,28 +3,29 @@
 """
 Level 2
 
-	----------------------------------------
+----------------------------------------
 
-	Congratulations.  You have reached level 2.
+Congratulations.  You have reached level 2.
 
-	To get the password for level 3, write code to find the first prime
-	fibonacci number larger than a given minimum.  For example, the first
-	prime fibonacci number larger than 10 is 13.
+To get the password for level 3, write code to find the first prime
+fibonacci number larger than a given minimum.  For example, the first
+prime fibonacci number larger than 10 is 13.
 
-	When you are ready, go here or call this automated
-	number (415) 799-9454.
+When you are ready, go here or call this automated
+number (415) 799-9454.
 
-	You will receive additional instructions at that time.  For the second portion
-	of this task, note that for the number 12 we consider the sum of the prime divisors
-	to be 2 + 3 = 5.  We do not include 2 twice even though it divides 12 twice.
+You will receive additional instructions at that time.  For the second portion
+of this task, note that for the number 12 we consider the sum of the prime divisors
+to be 2 + 3 = 5.  We do not include 2 twice even though it divides 12 twice.
 
+
+--notes
 the sum of the prime divisors of last answer plus 1 .... 514230
 
 2 3 5 61 281
 
 2 + 3 + 5 + 61 + 281
 Out[39]: 352  <--- ANSWER
-
 
 """
 
