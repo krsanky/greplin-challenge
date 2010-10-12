b472d8c30f4424e3b4533876c328a10768fcc02f
Paul Wisehart <paul@1ud2.com>
2010-10-12 11:20:24 -0400

    use the seqs as is instead of converting to str everywhere.

--- greplin-challenge/src/greplin_challenge/level1.clj
+++ greplin-challenge/src/greplin_challenge/level1.clj
@@ -9,43 +9,26 @@
 ;;
 ;; As an example, if the input was "I like racecars that go fast"
 ;; the password would be "racecar".
-;;
-;; ---- notes:
-;;  try leavinge the strings as seqs, to see if it's faster.
 
-(def getty-txt (.toLowerCase (slurp "gettysburg.txt")))
+(def *getty-txt* (.toLowerCase (slurp "gettysburg.txt")))
 
-(defn substrs-of-len [txt len]
-  "get all consecutive strings of length 'len' in 'txt'
-   returns a list of strings"
-  (map (fn [s] (apply str s)) (partition len 1 txt))) ;;change this to clojure's lambda shorthand ...
-
-(defn str-is-rev? [s] ;; what if s is nil ?
-  "tests if the string equals the reverse string, (and returns it if true)"
-  (let [rev-str (apply str (reverse s))]
-    (if (= s rev-str)
-      true
-      false)))
-
-(defn find-rev-str [txt len]
-  "find a reversible string of length 'len' in 'txt'"
-  (let [strs (substrs-of-len txt len)]
-    (let [ret (first (filter str-is-rev? strs))]
-      (println (str ret ":" len))
+(defn find-rev-seq [s len]
+  "find one reversible seq of length 'len' in 's'"
+  (let [sub-seqs (partition len 1 s)] ;;get all consecutive seqs of length 'len' in 's'
+    (let [ret (first (filter #(= % (reverse %)) sub-seqs))]
+      ;;(println (str ret ":" len))
       ret)))
 
-(defn longest-rev-str [txt]
-  "find the longest reversible substring"
-  (loop [len (count txt)]
-    (let [str (find-rev-str txt len)]
-      (if str
-        str
+(defn longest-rev-seq [s]
+  "find the longest reversible sub-seq"
+  (loop [len (count s)]
+    (let [sub-s (find-rev-seq s len)]
+      (if sub-s
+        sub-s
         (recur (dec len))))))
-(println (longest-rev-str getty-txt))
-;;(println (longest-rev-str "abba123"))
 
-;; (print (str getty-text "\n"))
-;; (substrs-of-len "asdasdasdABBAasdasdasd" 4)
-;; (print (str "count:" (count getty-text)))
+(defn run1 []
+  (println (longest-rev-seq *getty-txt*)))
+
 
 
