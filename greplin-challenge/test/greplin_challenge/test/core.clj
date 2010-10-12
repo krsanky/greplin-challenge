0dd4e25dbb44ccf666eea6edc126a60d3cabe678
Paul Wisehart <paul@1ud2.com>
2010-10-12 10:44:51 -0400

    rm unnused test code/dir

--- greplin-challenge/test/greplin_challenge/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns greplin-challenge.test.core
-  (:use [greplin-challenge.core] :reload)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
